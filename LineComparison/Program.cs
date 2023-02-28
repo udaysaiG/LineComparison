036468dea71a9985216a35efb6fcce2ef1fbab76
udaysaigalla <udaysaigalla8@gmail.com>
2023-02-28 23:57:40 +0530

    Geomety Length in Line Comparison

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to LineComparison");
+            // Console.WriteLine("Welcome to LineComparison");
+            LineComparison.Geometry_Length();
         }
     }
 }
